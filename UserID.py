d99cacb7a35b832c4311db3d1688a2decf2194e2
Rahil_Htree <124047785+rahilhtree@users.noreply.github.com>
2023-03-01 17:11:24 +0530

    Add User Id file
    this return the dictionary of name and id in the slackbot

---
+++ UserID.py
@@ -0,0 +1,30 @@
+import logging
+import slack_sdk
+from slack_sdk.errors import SlackApiError
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+users_store = {}
+client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
+
+# Put users into the dict
+def save_users(users_array):
+    for user in users_array:
+        # Key user info on their unique user ID
+        user_name = user['profile']['real_name']
+        # print(user['profile']['real_name'])
+        # Store the entire user object (you may not need all of the info)
+        users_store[user_name] = user["id"]
+try:
+    # Call the users.list method using the WebClient
+    # users.list requires the users:read scope
+    result = client.users_list()
+    save_users(result["members"])
+    print(users_store)
+
+except SlackApiError as e:
+    logger.error("Error creating conversation {}",format(e))

59ef0b1e60306993756d2af3f7367e1f5a160d51
Rahil_Htree <124047785+rahilhtree@users.noreply.github.com>
2023-03-06 11:30:33 +0530

    Flask API is Created

--- SlackBot.py
+++ SlackBot.py
@@ -1,6 +1,5 @@
 import logging
 import slack_sdk
-from slack_sdk.errors import SlackApiError
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -11,9 +10,10 @@ import calendar
 import constant
 import time as tm
 from UserID import user_id
+from flask import Flask,request
 
 
-
+app = Flask(__name__)
 # Setup the env file and Log
 logger = logging.getLogger(__name__)
 env_path = Path('.') / '.env'
@@ -23,6 +23,8 @@ load_dotenv(dotenv_path=env_path)
 client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
 LEAVE_API = os.environ['LEAVE_API']
 API_KEY = os.environ['API_KEY']
+
+
 LEAVE_CHANNEL= constant.LEAVE_CHANNEL
 GENERAL_CHANNEL= constant.GENERAL_CHANNEL
 BARKHA_ID= constant.BARKHA_ID
@@ -32,13 +34,9 @@ DHARMESH_ID= constant.DHARMESH_ID
 presentday = date.today() 
 currentYear = presentday.year
 
-announcement_id=[]
 
-# Leave time
-leaveTime = time(hour=12, minute=30)
-leaveScheduleTime = int(datetime.combine(presentday, leaveTime).timestamp())
 # BirthDay time
-birthdayTime = time(hour=5,minute=30)
+birthdayTime = time(hour=18,minute=10)
 birthdayScheduleTime = int(datetime.combine(presentday, birthdayTime).timestamp())
 
 # Work Anniversary time
@@ -61,7 +59,7 @@ def casualLeave(name,start_date,end_date,reason):
     if(start_date==end_date):
         return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be taking leave on {start_date} [{findDay(start_date)}].\n Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
     else:
-        return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be taking leave on {start_date} [{findDay(start_date)}] to {end_date} [{findDay(end_date)}].\n Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
+        return f"This is to inform you that,{f'<@{user_id[name]}>' if name in user_id else name} will be taking leave on {start_date} [{findDay(start_date)}] to {end_date} [{findDay(end_date)}].\n Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
 
 def earnedLeave(name,start_date,end_date,reason):
     if(start_date==end_date):
@@ -83,17 +81,18 @@ def adjustmentLeave(name,start_date,end_date,reason,adjustment_date):
     else:
        return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be taking adjustment leave on{start_date}[{findDay(start_date)}] to {end_date} [{findDay(end_date)}],\n will adjustment date:-{adjustment_date} \n Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
 
-def alternateShift(name,start_date,end_date,reason,alternate_shift):
+def altermateShift(name,start_date,end_date,reason,alternate_shift):
     if(start_date==end_date):
-       return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be doing alternate shift on {start_date}[{findDay(start_date)}].\n Alternate Shift Time :- {alternate_shift} \n Reason:-{reason}\n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
+       return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be doing alternate shift on {start_date}[{findDay(start_date)}].\n Alternate Shfit Time :- {alternate_shift} \n Reason:-{reason}\n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
     else:
-        return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be doing alternate shift on {start_date}[{findDay(start_date)}] to {end_date} [{findDay(end_date)}].\n Alternate Shift Time :-{alternate_shift} \n Reason:-{reason}\n  <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
+        return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be doing alternate shift on {start_date}[{findDay(start_date)}] to {end_date} [{findDay(end_date)}].\n Alternate Shfit Time :-{alternate_shift} \n Reason:-{reason}\n  <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
 
-def halfdayLeave(name,start_date,end_date,reason,half_leave_time):
+def halfdayLeave(name,start_date,end_date,reason):
     if(start_date==end_date):
-        return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be taking half-day leave on {start_date} [{findDay(start_date)}].\n Half-day Time:- {half_leave_time} \n Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
+        return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be taking half-day leave on {start_date} [{findDay(start_date)}].\n Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
     else:
-        return f"This is to inform you that, {f'<@{user_id[name]}>' if name in user_id else name} will be taking half-day leave on {start_date} [{findDay(start_date)}] to {end_date} [{findDay(end_date)}].\n Half-day Time:- {half_leave_time} \n  Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
+        return f"This is to inform you that,{f'<@{user_id[name]}>' if name in user_id else name} will be taking half-day leave on {start_date} [{findDay(start_date)}] to {end_date} [{findDay(end_date)}].\n Reason:- {reason} \n <!here> <@{BARKHA_ID}> <@{DHARMESH_ID}>"
+
 
 def birthdayWish(name):
     return f"<!channel> \n Today is {f'<@{user_id[name]}>' if name in user_id else name}'s birthday! \n From all of us here at the F(x), we wish you a very happy birthday & an  amazing year that ends with accomplishing all the great goals that you've set! \n May the days ahead be full of prosperity! \n \n Your contributions and commitment have made a significant impact on our success and we are grateful to have you as part of F(x) Team! \n Light the candles & uniquely enjoy this vital day using the fire of desires! Have a well deserved, thoroughly enjoyable birthday! \n Have a wonderful day! \n \n Once again, Happy Birthday! We look forward to continuing our work together and achieving more success in the future."
@@ -102,14 +101,6 @@ def birthdayWish(name):
 def workAnniversaryWish(name,totalYear):
     return f"<!channel> \nHappy work anniversary {f'<@{user_id[name]}>' if name in user_id else name}! \nWe want to take a moment to acknowledge and celebrate your {totalYear} work anniversary with F(x) Data Labs Pvt. Ltd. It's been an amazing journey working with you and we're fortunate to have someone with your talent and dedication on our team.\n\nYour hard work and commitment to excellence have not gone unnoticed. You have contributed significantly to the growth and success of our company, and we couldn't have come this far without your valuable contributions.\n\nWe appreciate your dedication, loyalty, and the positive impact you have made during your time with us. We are looking forward to working with you for many more years to come.\n\nCongratulations on this important milestone, and once again, thank you for everything you do."
 
-def announcementId(id):
-    for _id in announcement_id:
-        if(_id == id):
-            break;
-    else:
-        announcement_id.append(id)
-        return False
-    return True
 
 def switchType(type,result):
     name =result['name']
@@ -130,26 +121,8 @@ def switchType(type,result):
     elif type==5:
        return unpaidLeave(name,start_date,end_date,reason)
     elif type==6:
-        return alternateShift(name,start_date,end_date,reason,half_leave_time)
+        return altermateShift(name,start_date,end_date,reason,half_leave_time)
 
-
-def leave_schedule_messages():
-    try:
-        response = requests.get(f'{LEAVE_API}/slack-bot/leave-request?token={API_KEY}')
-        myresult =response.json()
-    except requests.exceptions.HTTPError as errh:
-            return "An Http Error occurred:" + repr(errh)
-    except requests.exceptions.ConnectionError as errc:
-        return "An Error Connecting to the API occurred:" + repr(errc)
-    except requests.exceptions.Timeout as errt:
-        return "A Timeout Error occurred:" + repr(errt)
-    except requests.exceptions.RequestException as err:
-            return "An Unknown Error occurred" + repr(err)
-    for result in myresult:
-        try:
-            client.chat_scheduleMessage(channel=LEAVE_CHANNEL,post_at=leaveScheduleTime,text=switchType(type=result['req_type_id'],result=result))
-        except SlackApiError as e:
-            logger.error("Error creating conversation {}",format(e))
 
 def birthday_schedule_message():
     try:
@@ -164,10 +137,7 @@ def birthday_schedule_message():
     except requests.exceptions.RequestException as err:
             return "An Unknown Error occurred" + repr(err)
     for result in myresult:
-        try:
-            client.chat_scheduleMessage(channel=GENERAL_CHANNEL,post_at=birthdayScheduleTime,text=birthdayWish(result['name']))
-        except SlackApiError as e:
-            logger.error("Error creating conversation {}",format(e))
+        client.chat_scheduleMessage(channel=GENERAL_CHANNEL,post_at=birthdayScheduleTime,text=birthdayWish(result['name']))
 
 def work_anniversary_schedule_message():
     try:
@@ -183,46 +153,36 @@ def work_anniversary_schedule_message():
         return "An Unknown Error occurred" + repr(err)
     for result in myresult:
         totalYear = currentYear - result['year']
-        try:
-            client.chat_scheduleMessage(channel=GENERAL_CHANNEL,post_at=workAnniversaryScheduleTime,text=workAnniversaryWish(result['name'],totalYear))
-        except SlackApiError as e:
-            logger.error("Error creating conversation {}",format(e))
+        client.chat_scheduleMessage(channel=GENERAL_CHANNEL,post_at=workAnniversaryScheduleTime,text=workAnniversaryWish(result['name'],totalYear))
 
+@app.route('/slack-bot/announcement',methods = ['POST'])
 def announcement_schedule_message():
-    try:
-        response = requests.get(f'{LEAVE_API}/slack-bot/announcement-request?token={API_KEY}')
-        myresult = response.json();
-    except requests.exceptions.HTTPError as errh:
-        return "An Http Error occurred:" + repr(errh)
-    except requests.exceptions.ConnectionError as errc:
-        return "An Error Connecting to the API occurred:" + repr(errc)
-    except requests.exceptions.Timeout as errt:
-        return "A Timeout Error occurred:" + repr(errt)
-    except requests.exceptions.RequestException as err:
-        return "An Unknown Error occurred" + repr(err)
-    announcementScheduleTime = int((datetime.now() + timedelta(minutes=1)).timestamp())
+    content = request.get_json();
+    result = content[0]
+    client.chat_postMessage(channel=GENERAL_CHANNEL,text=f"<!channel> \n{result['description']}")
+    return content
+
+@app.route('/slack-bot/leave-request',methods = ['POST'])
+def leave_schedule_messages():
+    content = request.get_json();
+    myresult = content[0]
     for result in myresult:
-        if(announcementId(result['id'])):
-            continue;
-        else:
-            try:
-                client.chat_scheduleMessage(channel=GENERAL_CHANNEL,post_at=announcementScheduleTime,text=f"<!channel> \n{result['description']}")
-            except SlackApiError as e:
-                logger.error("Error creating conversation {}",format(e))
+        client.chat_postMessage(channel=LEAVE_CHANNEL,text=switchType(type=result['req_type_id'],result=result))
+    return content  
+
+
 
 # Schedule Function to run everyday
 schedule.every().day.at("05:25").do(birthday_schedule_message)
-schedule.every().day.at("12:25").do(leave_schedule_messages)
 schedule.every().day.at("05:25").do(work_anniversary_schedule_message)
 
 
-# Run every minute
-schedule.every().minute.do(announcement_schedule_message)
+if(__name__== "__main__"):
+    app.run()
 
-
-# To run script infinite time
+# # To run script infinite time
 while True:
     # Checks whether a scheduled task
     # is pending to run or not
     schedule.run_pending()
-    tm.sleep(1)
+    tm.sleep(60)
